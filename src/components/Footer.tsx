1d8a10766ee77830bffdfab11eaf9525247969e3
FarLeaf <144786286+RafaelAngeloChristianto@users.noreply.github.com>
2025-08-14 21:11:17 +0700

    updates to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,70 @@
 import React from "react";
-
-
+import { SiWhatsapp, SiGmail } from "react-icons/si";
+import { Link } from "react-router-dom";
+import cali from "../assets/caligraphy_white.png";
 
 export const Footer: React.FC = () => {
-    return (
-        <>
-            <div className="w-full bg-[#152238] py-5">
-                <p className="text-center text-[#ffffff]">&copy; 2025 Rafael Angelo. All rights reserved.</p>
-            </div>
-        </>
-    )
-}
+  return (
+    <footer className="w-full bg-[#152238] py-6 flex flex-col items-center">
+      {/* Main Content */}
+      <div className="w-full max-w-5xl flex flex-col md:flex-row justify-between items-start md:items-center mb-4 px-4">
+        {/* Quick Links */}
+        <div>
+          <h3 className="text-white text-xl font-Roboto font-bold mb-3">
+            Quick Links
+          </h3>
+          <nav className="flex flex-col space-y-2">
+            <Link
+              className="text-white hover:underline hover:text-gray-300 transition-colors duration-200"
+              to="/"
+            >
+              Home
+            </Link>
+            <Link
+              className="text-white hover:underline hover:text-gray-300 transition-colors duration-200"
+              to="/certifications"
+            >
+              Certifications
+            </Link>
+          </nav>
+        </div>
+
+        <img src={cali} className="w-[200px]" alt="" />
+
+        {/* Contact */}
+        <div>
+          <h3 className="text-white text-xl font-Roboto font-bold mb-3">
+            Contact Me
+          </h3>
+          <div className="flex flex-col space-y-2">
+            <a
+              href="https://mail.google.com/mail/?view=cm&fs=1&to=rafaaelangelo@gmail.com"
+              className="flex items-center space-x-2 bg-white text-[#152238] px-3 py-2 rounded-md hover:bg-gray-200 transition-colors duration-200"
+            >
+              <SiGmail className="text-lg" />
+              <span>Contact via Email</span>
+            </a>
+            <a
+              href="https://wa.me/6281319727350?text=Hello%20Rafael%2C%20I%20want%20your%20service%20to%20create%20a%20website%20like%20..."
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center space-x-2 bg-green-600 text-white px-3 py-2 rounded-md hover:bg-green-500 transition-colors duration-200"
+            >
+              <SiWhatsapp className="text-lg" />
+              <span>Contact via WhatsApp</span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/* Divider */}
+      <hr className="w-full border-gray-500 mb-3" />
+
+      {/* Footer Bottom */}
+      <p className="text-center text-white text-sm mt-[5px] text-[15px] font-Inter">
+        &copy; {new Date().getFullYear()} Rafael Angelo Christianto. All rights
+        reserved.
+      </p>
+    </footer>
+  );
+};
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -8,11 +8,14 @@ import { Footer } from "../components/Footer";
 import portrait from "../assets/portrait.jpg";
 import cv from "../assets/Rafael Angelo - CV.pdf";
 
-import { SiInstagram } from "react-icons/si";
-import { SiTiktok } from "react-icons/si";
-import { SiLinkedin } from "react-icons/si";
-import { SiGithub } from "react-icons/si";
-import { SiWhatsapp } from "react-icons/si";
+import {
+  SiInstagram,
+  SiTiktok,
+  SiLinkedin,
+  SiGithub,
+  SiWhatsapp,
+  SiGmail,
+} from "react-icons/si";
 
 export const HomeScreen: React.FC = () => {
   const [imgLoaded, setImgLoaded] = React.useState(false);
@@ -21,6 +24,7 @@ export const HomeScreen: React.FC = () => {
     <div className="flex flex-col min-h-screen">
       <div className="flex flex-col min-h-screen scroll-smooth">
         <NavBar />
+
         {/* Hero Section */}
         <motion.div
           id="home"
@@ -30,24 +34,24 @@ export const HomeScreen: React.FC = () => {
           className="flex flex-col md:flex-row justify-center items-center flex-grow px-6 py-20 gap-10"
         >
           <div className="text-center md:text-left">
-            <h1 className="font-Inter font-bold text-[24px] md:text-[28px] mb-2 text-[#152238]">
+            <h1 className="font-Hammersmith-One font-bold text-[24px] md:text-[28px] mb-2 text-[#1F2A44]">
               Rafael Angelo Christianto
             </h1>
-            <h3 className="font-Open Sans text-[16px] md:text-[18px] text-gray-700">
+            <h3 className="font-Open-Sans text-[16px] md:text-[18px] text-gray-700">
               Penetration Tester & Full Stack Developer
             </h3>
-            <h4 className="font-Open Sans text-[14px] text-gray-600">
+            <h4 className="font-Open-Sans text-[14px] text-gray-600">
               Student at BINUS International University
             </h4>
 
             <div className="flex flex-col sm:flex-row sm:items-center sm:justify-start">
               <a target="_blank" href={cv} download="cv">
-                <button className="p-3 mt-4 rounded-[10px] bg-[#152238] text-white font-Inter hover:opacity-95 transition hover:cursor-pointer">
+                <button className="p-3 mt-4 rounded-[10px] bg-[#1F2A44] text-white font-Inter hover:opacity-95 transition hover:cursor-pointer">
                   Download CV
                 </button>
               </a>
               <Link to="/certifications">
-                <button className="p-3 mt-4 rounded-[10px] bg-white shadow-lg text-[#152238] font-Inter hover:opacity-95 transition hover:cursor-pointer">
+                <button className="p-3 mt-4 rounded-[10px] bg-white shadow-lg text-[#1F2A44] font-Inter hover:opacity-95 transition hover:cursor-pointer">
                   Certifications
                 </button>
               </Link>
@@ -60,7 +64,7 @@ export const HomeScreen: React.FC = () => {
             transition={{ duration: 0.6 }}
             onLoad={() => setImgLoaded(true)}
             decoding="async"
-            className="w-[250px] md:w-[350px] rounded-full hover:outline hover:outline-2 hover:outline-[#152238]"
+            className="w-[250px] md:w-[350px] rounded-full hover:outline hover:outline-2 hover:outline-[#1F2A44]"
             src={portrait}
             alt="Portrait"
           />
@@ -73,12 +77,12 @@ export const HomeScreen: React.FC = () => {
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
           transition={{ duration: 0.6 }}
-          className="px-6 py-12 text-justify max-w-[900px] w-full shadow-xl mb-[50px] mt-[50px] bg-white rounded-[20px]" // <-- removed self-center
+          className="px-6 py-12 text-justify max-w-[900px] w-full shadow-xl mb-[50px] mt-[50px] bg-white rounded-[20px]"
         >
-          <h1 className="text-2xl font-bold mb-4 text-[#152238] font-Inter">
+          <h1 className="text-2xl font-bold mb-4 text-[#1F2A44] font-Halant">
             About Me
           </h1>
-          <p className="text-base leading-relaxed text-gray-700">
+          <p className="text-base leading-relaxed text-gray-700 font-Nunito">
             My name is Rafael Angelo Christianto, currently pursuing a double
             degree in Computer Science at BINUS International University
             situated in Jakarta, Indonesia. I am currently studying in semester
@@ -88,6 +92,7 @@ export const HomeScreen: React.FC = () => {
             in what I do both individually or as a group.
           </p>
         </motion.div>
+
         {/* Education & Experience Section */}
         <motion.section
           initial={{ opacity: 0, y: 40 }}
@@ -98,30 +103,29 @@ export const HomeScreen: React.FC = () => {
         >
           {/* Education */}
           <div className="w-full max-w-[300px] shadow-xl px-5 py-12 rounded-[20px]">
-            <h1 className="text-2xl font-bold mb-4 text-[#152238] font-Inter">
+            <h1 className="text-2xl font-bold mb-4 text-[#1F2A44] font-Halant">
               Educations
             </h1>
-
             <div>
               <h2 className="font-Roboto">Lilin Bangsa Intercultural School</h2>
-              <h3>Science</h3>
-              <h4 className="text-gray-600 text-[14px]">
+              <h3 className="font-Open-Sans">Science</h3>
+              <h4 className="text-gray-600 text-[14px] font-Roboto">
                 July 2020 - May 2023
               </h4>
             </div>
             <br />
             <div>
               <h2 className="font-Roboto">BINUS International University</h2>
-              <h3>Computer Science</h3>
-              <h4 className="text-gray-600 text-[14px]">
+              <h3 className="font-Open-Sans">Computer Science</h3>
+              <h4 className="text-gray-600 text-[14px] font-Roboto">
                 September 2023 - June 2026
               </h4>
             </div>
             <br />
             <div>
               <h2 className="font-Roboto">RMIT University</h2>
-              <h3>Computer Science</h3>
-              <h4 className="text-gray-600 text-[14px]">
+              <h3 className="font-Open-Sans">Computer Science</h3>
+              <h4 className="text-gray-600 text-[14px] font-Roboto">
                 September 2026 - June 2027
               </h4>
             </div>
@@ -129,41 +133,45 @@ export const HomeScreen: React.FC = () => {
 
           {/* Experience */}
           <div className="w-full max-w-[300px] shadow-xl px-5 py-12 rounded-[20px]">
-            <h1 className="text-2xl font-bold mb-4 text-[#152238] font-Inter">
+            <h1 className="text-2xl font-bold mb-4 text-[#1F2A44] font-Halant">
               Experiences
             </h1>
-
             <div>
               <h2 className="font-Roboto">Student Committee</h2>
-              <h3>Lilin Bangsa Intercultural School</h3>
-              <h4 className="text-gray-600 text-[14px]">
+              <h3 className="font-Open-Sans">
+                Lilin Bangsa Intercultural School
+              </h3>
+              <h4 className="text-gray-600 text-[14px] font-Roboto">
                 September 2022 - May 2023
               </h4>
             </div>
             <br />
             <div>
               <h2 className="font-Roboto">Entrepreneurship Finalist</h2>
-              <h3>BINUS International University</h3>
-              <h4 className="text-gray-600 text-[14px]">
+              <h3 className="font-Open-Sans">BINUS International University</h3>
+              <h4 className="text-gray-600 text-[14px] font-Roboto">
                 September 2024 - January 2025
               </h4>
             </div>
             <br />
             <div>
               <h2 className="font-Roboto">Freshmen Partner</h2>
-              <h3>BINUS International University</h3>
-              <h4 className="text-gray-600 text-[14px]">
+              <h3 className="font-Open-Sans">BINUS International University</h3>
+              <h4 className="text-gray-600 text-[14px] font-Roboto">
                 September 2025 - May 2026
               </h4>
             </div>
             <br />
             <div>
               <h2 className="font-Roboto">Web Developer</h2>
-              <h3>Xlerator</h3>
-              <h4 className="text-gray-600 text-[14px]">July 2025 - Finish</h4>
+              <h3 className="font-Open-Sans">Xlerator</h3>
+              <h4 className="text-gray-600 text-[14px] font-Roboto">
+                July 2025 - Finish
+              </h4>
             </div>
           </div>
         </motion.section>
+
         {/* Projects Section */}
         <motion.div
           initial={{ opacity: 0, y: 40 }}
@@ -173,7 +181,7 @@ export const HomeScreen: React.FC = () => {
           className="mx-auto mb-[50px] shadow-xl w-full max-w-[900px] px-6 py-12 bg-[#F9FAFB] rounded-[20px]"
           id="projects"
         >
-          <h1 className="text-3xl font-bold mb-8 text-center text-[#152238] font-Inter">
+          <h1 className="text-3xl font-bold mb-8 text-center text-[#1F2A44] font-Halant">
             Projects
           </h1>
 
@@ -205,14 +213,14 @@ export const HomeScreen: React.FC = () => {
                 whileHover={{ scale: 1.02 }}
                 className="bg-white max-w-[400px] w-full p-6 rounded-[20px] shadow-md border border-gray-200"
               >
-                <h3 className="font-Hammersmith One text-[22px] font-bold text-[#152238] mb-1">
+                <h3 className="font-Hammersmith-One text-[22px] font-bold text-[#1F2A44] mb-1">
                   {proj.title}
                 </h3>
-                <h5 className="font-Open Sans text-gray-600 mb-4">
+                <h5 className="font-Open-Sans text-gray-600 mb-4">
                   Built with {proj.tech}
                 </h5>
                 <a target="_blank" href={proj.url}>
-                  <button className="bg-[#152238] hover:bg-[#1e2e4d] transition-colors text-white px-5 py-2 rounded-[20px] hover:cursor-pointer">
+                  <button className="bg-[#1F2A44] hover:bg-[#2B3C5A] transition-colors text-white px-5 py-2 rounded-[20px] font-Inter hover:cursor-pointer">
                     Visit Project
                   </button>
                 </a>
@@ -220,6 +228,7 @@ export const HomeScreen: React.FC = () => {
             ))}
           </div>
         </motion.div>
+
         {/* Contact Section */}
         <motion.div
           initial={{ opacity: 0, y: 40 }}
@@ -229,12 +238,13 @@ export const HomeScreen: React.FC = () => {
           className="shadow-xl font-Inter mb-[50px] px-6 py-6 rounded-[20px] bg-white w-full md:w-fit self-end"
           id="contact"
         >
-          <h1 className="text-2xl font-bold mb-2 font-Inter text-[#152238]">
+          <h1 className="text-2xl font-bold mb-2 font-Halant text-[#1F2A44]">
             Contact Me
           </h1>
 
-          <h4 className="text-sm text-gray-600 mb-2">
-            ✉️ rafaaelangelo@gmail.com
+          <h4 className="text-sm text-gray-600 mb-2 flex flex-row font-Roboto">
+            <SiGmail className="text-lg mr-[5px]" />
+            rafaaelangelo@gmail.com
           </h4>
 
           <a
@@ -247,7 +257,7 @@ export const HomeScreen: React.FC = () => {
             </button>
           </a>
 
-          <div className="flex flex-col gap-3 justify-center items-start mt-6 text-gray-700 text-sm">
+          <div className="flex flex-col gap-3 justify-center items-start mt-6 text-gray-700 text-sm font-Open-Sans">
             <a
               className="flex items-center gap-2"
               href="https://github.com/RafaelAngeloChristianto"
@@ -265,33 +275,35 @@ export const HomeScreen: React.FC = () => {
               <span>rafaaelangelo</span>
             </a>
             <a
+              className="flex items-center gap-2"
               href="https://www.instagram.com/rafaaelangelo"
               target="_blank"
-              className="flex items-center gap-2"
             >
               <SiInstagram className="text-lg" />
               <span>rafaaelangelo</span>
             </a>
             <a
+              className="flex items-center gap-2"
               href="https://www.tiktok.com/@rafaaelangelo"
               target="_blank"
-              className="flex items-center gap-2"
             >
               <SiTiktok className="text-lg" />
               <span>rafaaelangelo</span>
             </a>
             <a
+              className="flex items-center gap-2"
               href="https://wa.me/6281514383863?text=Hello%20Rafael%2C%20I%20want%20your%20service%20to%20create%20a%20website%20like%20..."
               target="_blank"
-              className="flex items-center gap-2"
             >
-              <SiWhatsapp className="text-lg text-[#B91C1C]" />
+              <SiWhatsapp className="text-lg" />
               <span>+62 813 1972 7350</span>
             </a>
           </div>
         </motion.div>
+
         <Footer />
       </div>
     </div>
   );
 };
+
