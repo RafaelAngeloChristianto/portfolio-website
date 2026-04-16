f6909149d381a6556ec152c5ab41037e58160529
Rafael Angelo <rafaaelangelo@gmail.com>
2026-04-16 22:11:11 +0700

    campfire

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -666,6 +666,12 @@ export const HomeScreen: React.FC = () => {
               difficulty:"Very Easy",
               category:"SOC",
               url:"/projects/BFT HTB Sherlock Writeup _ Rafael Angelo Christianto.pdf",
+            },
+            {
+              title:"Campfire-1",
+              difficulty:"Very Easy",
+              category:"DFIR",
+              url:"/projects/Campfire1 HTB Sherlock Writeup _ Rafael Angelo Christianto.pdf",
             }
           ].filter((s) => sherlockFilter === "All" || s.category === sherlockFilter).map((sherlock, i) => (
             <motion.a
